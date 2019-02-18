3380ec2f8befb8256c776dee538a9ccdb576c530
Hongkuan Wang <hongkuan.wang@aalto.fi>
2019-02-18 13:53:18 +0200

    deploy

--- gamestore/settings.py
+++ gamestore/settings.py
@@ -95,10 +95,10 @@ SOCIAL_AUTH_URL_NAMESPACE = 'social'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
